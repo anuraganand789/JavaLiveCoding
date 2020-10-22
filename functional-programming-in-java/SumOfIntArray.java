ad23082e40388eaacad8b4363c1ac5628087c523
Anurag Anand <anuraganand789@hotmail.com>
2020-10-22 11:52:11 +0530

    using block scope to group the logic, because initializing sum=0 everytime we reuse this variable does not clears the meaning that 'we are starting a different logical execution'

--- functional-programming-in-java/SumOfIntArray.java
+++ functional-programming-in-java/SumOfIntArray.java
@@ -5,22 +5,28 @@ public class SumOfIntArray{
     public static void main(String ...args){
         final int[] ints = {1, 2, 3, 4, 5, 6};
 
-	int sum = 0;
-	for(int intValue : ints) sum += intValue;
-	System.out.println("Tradiiontal Sum " + sum);
-	
-	sum = 0;
+	{
+	    int sum = 0;
+	    for(int intValue : ints) sum += intValue;
+	    System.out.println("Tradiiontal Sum " + sum);
+        }	
 
-	sum = Arrays.stream(ints).sum();
-	System.out.println("Functional Stream 'sum' function Sum " + sum);
+	{
+	    int sum = Arrays.stream(ints).sum();
+	    System.out.println("Functional Stream 'sum' function Sum " + sum);
+	}
 
-	sum = 0;
-
-	sum = Arrays.stream(ints).reduce(0, Integer::sum);
-	System.out.println("Functional Stream 'reduce' function Sum " + sum);
+	{
+	    int  sum = Arrays.stream(ints).reduce(0, Integer::sum);
+	    System.out.println("Functional Stream 'reduce' function Sum " + sum);
+        }
 
 	final AtomicInteger sumInt = new AtomicInteger(0);
-	Arrays.stream(ints).forEach( intValue -> sumInt.set( intValue + sumInt.get()));
+
+	Arrays
+	.stream(ints)
+	.forEach(intValue -> sumInt.set( intValue + sumInt.get()));
+
 	System.out.println("Functional Stream should not be done this way " + sumInt.get()); 
 
     }
