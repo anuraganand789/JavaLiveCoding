7fa4eaa237df8d199cb8a16d9e12e40a996c0183
Anurag Anand <anuraganand789@hotmail.com>
2020-10-22 11:50:29 +0530

    added new line

--- functional-programming-in-java/Currying.java
+++ functional-programming-in-java/Currying.java
@@ -27,6 +27,7 @@ public class Currying {
 
     public static void main(String ...args){
          final Currying               curry                     = new Currying();
+
 	 final UnaryOperator<Integer> addTwoCurry               = curry.adderGenerator(2);
 	 final UnaryOperator<Integer> multiplyBy_100            = curry.multiplierGenerator(100);
          
@@ -37,9 +38,11 @@ public class Currying {
 
 	 printToConsole.accept("Add Two to 2 = " + addTwoCurry.apply(2));
 	 printToConsole.accept("Add Two to 3 = " + addTwoCurry.apply(3));
+
 	 printToConsoleNoNewLine.accept("Add Two to 4 = " + addTwoCurry.apply(4));
 
 	 printToConsole.accept("Add Two to 2 = " + multiplyBy_100.apply(2));
+
 	 designerDisplay.accept("Add Two to 3 = " + multiplyBy_100.apply(3));
 	 designerDisplay.accept("Add Two to 4 = " + multiplyBy_100.apply(4));
     }
