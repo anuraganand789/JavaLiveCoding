16fcc85082a025760cd84828956e7e00399265b1
Anurag Anand <anuraganand789@hotmail.com>
2021-03-17 00:05:03 +0530

    containes modular code for a single module

---
+++ modules/single-module-demo/module-source/test.module/test/Test.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Test{
+    
+    public static void main(final String ...args){
+        System.out.println("***Hello from test.Test class inside the test.module***");
+    }
+}
